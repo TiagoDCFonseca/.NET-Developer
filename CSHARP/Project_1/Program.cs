0d1d6d1807090b0ccd0b254b1d99b5296f6e03ad
TiagoDCFonseca <139363883+TiagoDCFonseca@users.noreply.github.com>
2024-05-15 12:43:19 +0100

    Teste C#

---
+++ CSHARP/Project_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Project_1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Escrever Linhas no ecrã e mudar a cor
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Exemplo de uma linha");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Exemplo de mais uma linha");
+        }
+    }
+}
