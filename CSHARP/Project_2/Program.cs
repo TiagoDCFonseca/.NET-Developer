369656a3bb66cb68bc1a459297d0c096329b9e22
TiagoDCFonseca <139363883+TiagoDCFonseca@users.noreply.github.com>
2024-05-15 14:54:10 +0100

    Project2 Upload

--- CSHARP/Project_2/Program.cs
+++ CSHARP/Project_2/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Project_2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Variávei
+            int idade = 0;
+            string nome = "";
+
+            //Escreve no ecrâ
+            Console.WriteLine("Insira o seu nome:");
+            //Guarda o input do utilizador na variável nome
+            nome = Console.ReadLine();
+
+            //Como o valor que pretendemos guardar é um número 
+            //por isso convertemos antes de guardar na variável
+            Console.WriteLine("Insira a sua idade:");
+            idade = int.Parse(Console.ReadLine());
+
+            //Quebras de linha
+            Console.WriteLine("\n\n");
+
+            //Formas de mostrar variáveis no ecrâ
+            Console.WriteLine("O teu nome é " + nome + " e tens " + idade + " anos de idade.");
+            Console.WriteLine("O teu nome é {0} e tens {1} anos de idade.", nome, idade);
+            Console.WriteLine($"O teu nome é {nome} e tens {idade} anos de idade.");
+        }
+    }
+}
