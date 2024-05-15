1822a4b407be3cf821778281ad41c9c7ada3c814
TiagoDCFonseca <139363883+TiagoDCFonseca@users.noreply.github.com>
2024-05-15 16:18:22 +0100

    Project3

---
+++ CSHARP/Project_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Project_3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int num1 = 0, num2 = 0;
+
+            Console.WriteLine("Insira o primeiro número.");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insira outro número.");
+            num2 = int.Parse(Console.ReadLine());
+
+            int soma = num1 + num2;
+
+            Console.WriteLine($"A soma de {num1} + {num2} é igual a {soma}." );
+            Console.WriteLine($"A soma de {num1} + {num2} é igual a {num1 + num2}.");
+            
+        }
+    }
+}
