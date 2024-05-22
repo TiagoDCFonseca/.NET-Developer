60f37838be606aba538fc0104a6d7cca11823db6
TiagoDCFonseca <139363883+TiagoDCFonseca@users.noreply.github.com>
2024-05-22 12:16:05 +0100

    Project9 finished
    begin project 10

---
+++ CSHARP/Project_9/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project9
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {   // Estruturas de decisão:
+            // E  --> &&
+
+            int numGerado = 0;
+            Random aleatorio = new Random();
+            numGerado = aleatorio.Next(1, 101);
+
+            Console.WriteLine($"O número gerado foi: {numGerado}");
+
+            if (numGerado >= 30 && numGerado <= 70)
+            {
+                Console.WriteLine($"O número {numGerado} está entre 30 e 70");
+             
+            }
+            else if (numGerado < 30)
+            {
+                Console.WriteLine($"O número {numGerado} é menor que 30");
+            }
+            else
+            {
+                Console.WriteLine($"O número {numGerado} é maior 70");
+
+            }
+        }
+    }
+}
