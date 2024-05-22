dbd2caf2fce1d0b791de131195e2bb56ded3b8fb
TiagoDCFonseca <139363883+TiagoDCFonseca@users.noreply.github.com>
2024-05-22 12:41:03 +0100

    Project 11 Finished

--- CSHARP/Project_11/Program.cs
+++ CSHARP/Project_11/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project11
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Utilizar o switch para testar condições
+            //com valores inteiros.
+
+            int num;
+            Console.WriteLine("Escolhe um número entre 1 e 5");
+            num = int.Parse(Console.ReadLine());
+
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("Escolheste o número UM");
+                    break;
+                case 2:
+                    Console.WriteLine("Escolheste o número DOIS");
+                    break;
+                case 3:
+                    Console.WriteLine("Escolheste o número TRÊS");
+                    break;
+                case 4:
+                    Console.WriteLine("Escolheste o número QUATRO");
+                    break;
+                case 5:
+                    Console.WriteLine("Escolheste o número CINCO");
+                    break;
+                default:
+                    Console.WriteLine("Escolha um número entre 1 e 5");
+                    break;
+            }
+        }  
+    }
+}
