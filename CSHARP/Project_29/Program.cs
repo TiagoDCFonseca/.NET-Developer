8f5314058151329db51f9e580a9d8d8fb8c5c99e
TiagoDCFonseca <139363883+TiagoDCFonseca@users.noreply.github.com>
2024-05-27 14:39:39 +0100

    Finished

---
+++ CSHARP/Project_29/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_29
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            /* Programa que pede ao utilizador para inserir 5 números ímpares,
+             * caso o utilizador insira pares a aplicação 
+             * não deverá aceitar e pede um novo número ao utilizador.
+             * Mostrar os números no ecran.
+             */
+
+            int numero = 0;
+            string num_Texto = "";
+
+            Console.WriteLine("Programa de números Impares");
+            for (int i = 0; i < 5; i++)
+            {
+                numero = 2;
+                while (numero % 2 == 0)
+                {
+                    Console.Write($"Insira o {i} número Impar: ");
+                    numero = int.Parse(Console.ReadLine());
+                }
+                num_Texto = num_Texto + numero.ToString() + " ";
+            }
+            Console.WriteLine($"\n{num_Texto}");
+        }
+    }
+}
