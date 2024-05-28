0b994b3b874a213025eec0db5e2745765a47e46e
TiagoDCFonseca <139363883+TiagoDCFonseca@users.noreply.github.com>
2024-05-28 15:40:48 +0100

    Project36 Função FraseReversa

---
+++ CSHARP/Project_36/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_36
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Função recebe frase e retorna a frase ao contrário
+            string frase_Original = "";
+            Console.WriteLine("Escreva uma frase.");
+            frase_Original = Console.ReadLine();
+
+            Console.WriteLine(Frase_Reversa(frase_Original));
+        }
+        static string Frase_Reversa(string frase)
+        {
+            string frase_Invertida = "";
+            for (int i = frase.Length - 1; i >= 0; i--)
+            {
+                frase_Invertida += frase.Substring(i, 1);
+            }
+            return frase_Invertida;
+        }  
+    }
+}
