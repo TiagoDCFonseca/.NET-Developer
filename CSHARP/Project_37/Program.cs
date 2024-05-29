856e918d12c235d547135cf8403f2c456b50eb90
TiagoDCFonseca <139363883+TiagoDCFonseca@users.noreply.github.com>
2024-05-29 09:54:02 +0100

    Introdução a arrays números

---
+++ CSHARP/Project_37/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_37
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Introdução aos Arrays
+
+            int[] valores = new int[5]; // Array com tamanho de 5 posições
+
+            // Atribuir valores a cada posição
+            valores[0] = 10;
+            valores[1] = 20;
+            valores[2] = 30;
+            valores[3] = 40;
+            valores[4] = 50;
+
+            // Percorre o array e escreve no ecrâ
+
+            for(int i = 0; i < valores.Length; i++)
+            {
+                Console.Write($"{valores[i]} ");
+            }      
+        }
+    }
+}
