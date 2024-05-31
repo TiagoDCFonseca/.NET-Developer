43cf290bfda7515bf67242a1c4217e4f7f4b1464
TiagoDCFonseca <139363883+TiagoDCFonseca@users.noreply.github.com>
2024-05-31 15:40:57 +0100

    Escrever só vogais de uma frase usando array de carcteres

---
+++ CSHARP/Project_46/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_46
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Exemplo de carregar vogais para dentro de um array
+
+            char[] vogais = { 'a', 'e', 'i', 'o', 'u', 'á', 'à', 'é', 'í', 'ó', 'ú', 'ã', 'â', 'ê', 'õ'};
+
+            string frase = "Olá turma de Programação";
+
+            for (int i = 0; i < frase.Length; i++)
+            {
+                if(vogais.Contains(frase[i]))
+                {
+                    Console.Write(frase[i]);
+                }
+            }
+        }
+    }
+}
